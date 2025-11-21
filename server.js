d3c866504e352e8d73cbe06172fdb10d9f7e242c
cassieee0914 <cassieee0914@gmail.com>
2025-11-21 15:45:43 +0000

    Update message

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.write("Ma. Cassandra E. Matosinos");
+   res.write("BA- 4102");
+    res.write("Respect yourself enough to walk away from anything that no longer grows you.");
   res.end();
 });
 
